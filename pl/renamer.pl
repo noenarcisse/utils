a50039cdc3748ce58a1f9a823772dfccb28ed510
NoeN <narcisse.noe@gmail.com>
2026-01-09 10:12:38 +0100

    Create renamer.pl
    Added pl folder
    Added rename.pl

---
+++ pl/renamer.pl
@@ -0,0 +1,44 @@
+#renamer.pl
+
+use strict;
+use warnings;
+
+
+my $matches = '';
+
+my @files = glob('*');
+
+sub findRename
+{
+	my $s = $_[0];
+	my $r = $_[1];
+	
+	die "findRename: perl findRename.pl search [replace]" if !defined $s;
+
+		
+	foreach my $file (@files)
+	{
+		next if $file eq 'renamer.pl';
+		
+		my $newName = $file;
+
+		
+		if($file =~ /$s/gi)
+		{
+				if(defined $r)
+				{
+					$newName =~ s/$s/$r/i;
+					$matches .= "REPLACE: ${ file } with ${ newName} \n";
+					rename($file, $newName);
+				}
+				else
+				{
+					$matches .= "FOUND: ${ file } \n";
+				}
+		}
+	}
+
+	print $matches;
+}
+
+findRename(@ARGV);
