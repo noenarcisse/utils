4b9236b2bcf3a8679c230c52b40fbd158f232303
NoeN <narcisse.noe@gmail.com>
2026-01-31 12:00:22 +0100

    Create Color.ts

---
+++ ts/Color.ts
@@ -0,0 +1,255 @@
+/**
+ * Color object with possible manipulation of a color
+ * @class
+ */
+class Color
+{
+	r : number;
+	g : number;
+	b : number;
+	a : number;
+	/**
+	 * Constructor
+	 * @param arg1 - The source of the color.
+	 * Can be:
+	 * - The red channel
+	 * - A string representing the CSS string rgb(), rgba(), #FF00FF
+	 * - An array of numbers with or without alpha such as [255,0,255,0.5]
+	 * @param arg2 - The green channel of the color
+	 * @param arg3 - The blue channel of the color
+	 * @param arg4 - The alpha channel of the color
+	 */
+	constructor(arg1:number|string|Array<number>, arg2?:number, arg3?:number, arg4?:number)
+	{
+		let r:number | undefined;
+		let g:number | undefined;
+		let b:number | undefined;
+		let a:number = arg4 ?? 1; //Quadruple or default
+		
+		// TRIPLE
+		if (typeof arg3 === 'number' && typeof arg2 === 'number' && typeof arg1 === 'number') {
+			// R G B		
+			r = arg1;
+			g = arg2;
+			b = arg3;
+		}
+		
+		//MONO
+		if(typeof arg2 === 'undefined')
+		{
+			if(typeof arg1 === 'string')
+			{
+				let col: string[] = [];
+
+				//string rgba (255, 255, 255)
+				//attention le [\s,]+ accepte possiblement : "255 , , 255"
+				let regRGBA : RegExp = /^rgba\(\s*[0-9]{1,3}[\s,]+[0-9]{1,3}[\s,]+[0-9]{1,3}[\s,]+[0-9.]+\s*\)$/i;
+				let reg : RegExp =  /^rgb\(\s*[0-9]{0,3}[\s,]+[0-9]{0,3}[\s,]+[0-9]{1,3}\s*\)$/i;
+
+				if (regRGBA.test(arg1)) {
+					col = arg1.match(/\d+\.?\d*/g)!; // 4 deci avec . possible
+					
+					r = parseInt(col[0]);
+					g = parseInt(col[1]);
+					b = parseInt(col[2]);
+					
+					// On vérifie si l'alpha extrait (col[3]) est valide
+					if (col[3].match(/^[01](\.[0-9]+)?$/)) {
+						a = parseFloat(col[3]);
+					} else {
+						throw new Error("Alpha value must be a float between 0 and 1");
+					}
+				}
+				else if(reg.test(arg1)) //string rgb (255, 255, 255)
+				{
+					col = arg1.match(/[0-9]{1,3}/g)!;
+					
+					r = parseInt(col[0]);
+					g = parseInt(col[1]);
+					b = parseInt(col[2]);
+				}
+				//string #ffffff
+				reg = /^[#][0-9a-fA-F]{6}/g;
+				if(reg.test(arg1))
+				{
+					col = arg1.match(/[0-9a-fA-F]{2}/g)!;
+					r = parseInt(col[0],16);
+					g = parseInt(col[1],16);
+					b = parseInt(col[2],16);
+				}
+
+				if(!col)
+					throw new Error("The color format cannot be parsed. Color as a string must be either rgb(), rgba() or an HEXA value");
+
+			}
+			
+			if(Array.isArray(arg1)) 			//array
+			{
+				//array a 3 +
+				if(arg1.length >= 3)
+				{
+					r = arg1[0];
+					g = arg1[1];
+					b = arg1[2];
+					
+					//array a 4
+					a = (arg1.length == 4) ? arg1[3] : 1;
+				}
+
+			}
+		}
+
+		if (typeof a === 'undefined' ||
+			typeof r === 'undefined' ||
+			typeof g === 'undefined' ||
+			typeof b === 'undefined'
+		) {
+			throw new Error("ERREUR DE COULEUR");
+		}
+		
+
+		if (!(isColorValue(r) && isColorValue(g) && isColorValue(b))) 
+			throw new Error("RGB (" + r + " " + g + " " + b + ") values are not [0-255]");
+		
+		if(!isAlphaValue(a))
+			throw new Error("Alpha value is not [0-1]");
+
+		// faut throw ICI pour l'erreur final si on a pas pu creer la couleur en général en polymorphe
+
+
+
+		this.a = a;
+		this.r = r;
+		this.g = g;
+		this.b = b;
+		
+		
+		function isColorValue(n:number)
+		{
+			return ((n >= 0) && (n < 256));
+		}
+		function isAlphaValue(a:number)
+		{
+			return ((a >= 0) && (a <= 1));
+		}
+	}
+
+	add()
+	{
+
+	}
+	sub()
+	{
+
+	}
+
+	/**
+	 * Swizzle the channels of a color based on the pattern
+	 * @param {string} pattern - A pattern of rgba letters with the correct order of the swizzle
+	 * @returns {this} - The current instance of Color for chaining purpose
+	 */
+	swizzle(pattern:string) 
+	{
+		const old : {[key:string]: number} = {
+			r: this.r,
+			g: this.g,
+			b: this.b,
+			a: this.a * 255 //normalisation temp de channel A
+		};
+
+		const channels = pattern.toLowerCase().split("");
+
+		// 2. Swizzling : les valeurs s'échangent sans perte d'échelle
+		if (channels.length > 0) this.r = old[channels[0]] ?? old.r;
+		if (channels.length > 1) this.g = old[channels[1]] ?? old.g;
+		if (channels.length > 2) this.b = old[channels[2]] ?? old.b;
+
+		let newAlphaIn255;
+		if (channels.length > 3) 
+		{
+			newAlphaIn255 = old[channels[3]] ?? (this.a * 255);
+		} 
+		else 
+		{
+			newAlphaIn255 = old.a;
+		}
+
+		// retour a un alpha float entre 0 - 1
+		this.a = newAlphaIn255 / 255;
+
+		this.r = Math.round(this.r);
+		this.g = Math.round(this.g);
+		this.b = Math.round(this.b);
+
+		return this;
+	}
+
+	/**
+	 * Lighten the color
+	 * @param {number} add - The absolute number to add to the overall Color luminance
+	 */
+	lighten(add:number)
+	{
+		add=Math.abs(add);
+		this.r = this.clampColor(this.r+add);
+		this.g = this.clampColor(this.g+add);
+		this.b = this.clampColor(this.b+add);
+	}
+	/**
+	 * Darken the color
+	 * @param {number} sub - The absolute number to subtract from the overall Color luminance
+	 */
+	darken(sub:number)
+	{
+		sub=Math.abs(sub);
+		this.r = this.clampColor(this.r-sub);
+		this.g = this.clampColor(this.g-sub);
+		this.b = this.clampColor(this.b-sub);
+	}
+
+	clampColor(c:number)
+	{
+		if(c > 255) c=255;
+		if(c < 0) c=0;
+		return c
+	}
+
+	/**
+	 * Returns a string of the color
+	 * @returns {string} - The string of the color in a format : "R:x G:x B:x A:x"
+	 */
+	toString()
+	{
+		return 'R:'+this.r+' G:'+this.g+' B:'+this.b+' A:'+this.a;
+	}
+	/**
+	 * Returns the RGB value of the color
+	 * @returns {string} - A CSS rgb format
+	 */
+	toRgb()
+	{
+		return `rgb(${this.r},${this.g},${this.b})`;
+	}
+	/**
+	 * Returns the RGBA value of the color
+	 * @returns {string} - A CSS rgba format
+	 */
+	toRgba()
+	{
+		return `rgb(${this.r},${this.g},${this.b},${this.a})`;
+	}
+	/**
+	 * Returns the hexadecimal value of the color
+	 * @returns {string} - A CSS hexadecimal format
+	 */
+	toHex()
+	{
+		const _r = Math.round(this.r).toString(16).padStart(2, '0');
+		const _g = Math.round(this.g).toString(16).padStart(2, '0');
+		const _b = Math.round(this.b).toString(16).padStart(2, '0');
+
+		return `#${_r}${_g}${_b}`.toUpperCase();
+	}
+}
+
+export default Color;
