b73799dff9812101722ad1752b3a01ff5aaa68d0
NoeN <24297098+noenarcisse@users.noreply.github.com>
2026-07-27 20:24:32 +0200

    Update squeal.nim

--- nim/squeal.nim
+++ nim/squeal.nim
@@ -5,46 +5,33 @@ import std/dirs
 import std/files
 
 var sql_tokens = @[
-    "select","from","as",
-    "where",
+    "select","from","as","where",
     "order","group","by",
     "asc","desc",
     "count","sum",
     "insert","into","values",
-    "delete",
-    "update",
-    "concat",
-    "distinct",
-    "like",
-    "ilike",
-    "cast",
-    "extract",
-    "to_char",
-    "position",
-    "substring", "upper","lower","replace", "trim","rtrim","ltrim",
+    "delete","update",
+    "concat","distinct",
+    "like","ilike",
+    "cast","extract","to_char",
+    "position","substring", "upper","lower","replace", "trim","rtrim","ltrim",
     "count", "max","min","avg","case","when","else","end","nullif","coalesce"
     ].toHashSet
 
-# open file, rw
 var f, f2 : File
 var fs : seq[string]
 
 let wd = getCurrentDir()
 for e in walkDir(wd) :
-
     if e.kind != pcFile : continue
     let (_,filename,ext) = splitFile(e.path)
     if ext != ".sql" : continue
     fs.add $filename&ext
 
-
 for file in fs :
-
-    let fp = $file #repassage en string leggit
-    if open(f, fp, FileMode.fmRead) :
+    if open(f, file, FileMode.fmRead) :
         # non defer, il est fermé plus loin, trust me
-        if open(f2, "tmp_"&fp, FileMode.fmWrite) : echo "Nice"
-            
+        if open(f2, "tmp_"&file, FileMode.fmWrite) : echo "Nice"           
 
         try :
             var currentWord : seq[char]
@@ -53,19 +40,14 @@ for file in fs :
                 currentWord = @[]
                 let l = f.readLine()
 
-                echo l
-
                 for c in l :
                     #  parsing here
-                    # todo
                     if c in Whitespace :
                         var wordStr = currentWord.join()
 
                         if wordStr in sql_tokens :
-                            echo "Found : ", wordStr
                             f2&=wordStr.toUpperAscii()
                         else :
-                            echo wordStr
                             f2.write wordStr
                         f2&=c 
                         # on vide quand finito
@@ -76,21 +58,15 @@ for file in fs :
             # dernier mot on touche possiblement EOF, faut verifier si il est dedans
             if currentWord.join() in sql_tokens :
                 var wordStr = currentWord.join()
-                echo "last not taken : ", wordStr
                 f2&=wordStr.toUpperAscii()
-
         except :
             echo "oops"
         finally :
+            #safe?
             f.close()
             f2.close()          
     else :
         echo "Impossible d'ouvrir le fichier"
 
-    #move here
-    moveFile(Path("tmp_"&fp), Path(fp))
+    moveFile(Path("tmp_"&file), Path(file))
 #end foreach files
-            
-# la meilleure solution c'est
-# ouvrir un autre fichier, scan par line, parse la line curr, ecrire le fichier 2 avec remplacement sur les tokens
-# puis move fichier_2_tmp -> fichier1
