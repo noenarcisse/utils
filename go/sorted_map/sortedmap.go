0beb031c42a5fbb8f1ca0be9c3e8fc96cacbb454
NoeN <24297098+noenarcisse@users.noreply.github.com>
2026-08-30 14:29:05 +0200

    Update sortedmap.go

--- go/sorted_map/sortedmap.go
+++ go/sorted_map/sortedmap.go
@@ -7,8 +7,9 @@ import (
 )
 
 type SortedMap[T cmp.Ordered, U any] struct {
-	inner map[T]U
-	order []T
+	inner  map[T]U
+	order  []T
+	sorted bool
 }
 
 func NewSortedMap[T cmp.Ordered, U any]() *SortedMap[T, U] {
@@ -20,6 +21,7 @@ func NewSortedMap[T cmp.Ordered, U any]() *SortedMap[T, U] {
 
 func (s *SortedMap[T, U]) sort() {
 	slices.Sort(s.order)
+	s.sorted = true
 }
 
 func (s *SortedMap[T, U]) Add(key T, val U) {
@@ -27,10 +29,15 @@ func (s *SortedMap[T, U]) Add(key T, val U) {
 		s.order = append(s.order, key)
 	}
 	s.inner[key] = val
+	s.sorted = false
 }
 
 func (s *SortedMap[T, U]) Items() iter.Seq2[T, U] {
-	s.sort()
+
+	if !s.sorted {
+		s.sort()
+	}
+
 	return func(yield func(T, U) bool) {
 		for _, e := range s.order {
 			if !yield(e, s.inner[e]) {
